c33564ca255c7bd1b9f74f86098a62a6c3be6183
Louix <lgraafmans7@gmail.com>
2025-08-29 14:16:30 +0200

    Fix GitHub Actions workflow to use GITHUB_TOKEN instead of ACCESS_TOKEN

--- generate_images.py
+++ generate_images.py
@@ -101,7 +101,8 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
+        access_token = os.getenv("GITHUB_TOKEN")
+    if not access_token:
         raise Exception("A personal access token is required to proceed!")
     user = os.getenv("GITHUB_ACTOR")
     if user is None:
